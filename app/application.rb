12c13c0c916fe500c3dc64cf9dcf5e89abcbde41
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-27 13:32:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    # resp.write "Hello, World"
+    # resp.finish
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
